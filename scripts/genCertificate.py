125afd4d163b8b116900fefa119615f545a25979
Sajal Gupta <gupta.sajal631@gmail.com>
2020-12-27 00:24:30 +0530

    Add genConfig

--- scripts/genCertificate.py
+++ scripts/genCertificate.py
@@ -2,6 +2,13 @@ from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import os
 
+# Interfaced customization
+# Names and Texts to be picked up from text File
+# Configurable Text fields from config_certificate_id
+# create box to fill the text and value.
+# If text size overflow from box size then reduce text size and name shortening.
+# Datasheet pass on runtime
+
 name = "Sajal Gupta"
 font = ImageFont.truetype('GreatVibes-Regular.ttf', 96)
 img = Image.open('/Users/salroid/Documents/GitHub/Dynamic-Certificate-Generator/certificates/4571.jpg')
