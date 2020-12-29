0e291ead129b7cd61159f1599bc58ed3e98661f1
Sajal Gupta <gupta.sajal631@gmail.com>
2020-12-29 11:56:29 +0530

    Update getCoordinates.py

--- scripts/getCoordinates.py
+++ scripts/getCoordinates.py
@@ -1,50 +1,21 @@
-import cv2
 import sys
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-def click_event(event, x, y, flags, params): 
-    # checking for left mouse clicks 
-    if event == cv2.EVENT_LBUTTONDOWN: 
-        # displaying the coordinates 
-        # on the Shell 
-        print(x, ' ', y) 
+def onclick(event): 
+    print("button=%d, x=%d, y=%d, xdata=%f, ydata=%f" % ( 
+         event.button, event.x, event.y, event.xdata, event.ydata)) 
 
-        # displaying the coordinates 
-        # on the image window 
-        font = cv2.FONT_HERSHEY_SIMPLEX 
-        cv2.putText(img, str(x) + ',' + str(y), (x,y), font, 1, (255, 0, 0), 2) 
-        cv2.imshow('image', img) 
+certificateName = sys.argv[1]
+certificateId = certificateName.split('.')[0]
+path = '/Users/salroid/Documents/GitHub/Dynamic-Certificate-Generator/'
+pathCertificate = path + 'certificates/' + certificateId + '.jpg'
 
-    # checking for right mouse clicks      
-    if event==cv2.EVENT_RBUTTONDOWN: 
-        # displaying the coordinates 
-        # on the Shell 
-        print(x, ' ', y) 
-        # displaying the coordinates 
-        # on the image window 
-        font = cv2.FONT_HERSHEY_SIMPLEX 
-        b = img[y, x, 0] 
-        g = img[y, x, 1] 
-        r = img[y, x, 2] 
-        cv2.putText(img, str(b) + ',' + str(g) + ',' + str(r), (x,y), font, 1, (255, 255, 0), 2) 
-        cv2.imshow('image', img) 
+img = mpimg.imread(pathCertificate)
+fig, dx = plt.subplots()
+dx.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+ax = plt.imshow(img)
+fig = ax.get_figure()
 
-if __name__=="__main__": 
-    # reading the image 
-    certificateName = sys.argv[1]
-    certificateId = certificateName.split('.')[0]
-    path = '/Users/salroid/Documents/GitHub/Dynamic-Certificate-Generator/'
-    pathCertificate = path + 'certificates/' + certificateId + '.jpg'
-
-    img = cv2.imread(pathCertificate, 1) 
-    # displaying the image 
-    cv2.imshow('image', img); 
-    cv2.waitKey(0)
-    # setting mouse hadler for the image 
-    # and calling the click_event() function 
-    #cv2.setMouseCallback('image', click_event) 
-    # wait for a key to be pressed to exit 
-  
-    # close the window 
-    cv2.imshow('image', img); cv2.waitKey(0)
-    
-    cv2.waitKey(1)
+cid = fig.canvas.mpl_connect('button_press_event', onclick) 
+plt.show()
